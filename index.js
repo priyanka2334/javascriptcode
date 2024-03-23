53fc85a1d350024413d6bdaf6bc22af2b42e6743
priyanka2334 <priyankadiwakar2024@gmail.com>
2024-03-23 22:53:33 +0530

    commit

---
+++ index.js
@@ -0,0 +1,122 @@
+
+// 1. Take two number num,num1 and print the sum diffrence quotient and modulus of a & b ?
+
+// const prompt = require('prompt-sync')();
+// let num = parseInt(prompt("Enter the number"));
+// let num1= parseInt(prompt("Enter the number1"));
+// console.log(num + num1);
+// console.log(num % num1);
+// console.log(num * num1);
+// console.log(num - num1);
+// console.log(num / num1);
+
+
+// 2.Take two numbers and only print the integer part when a is divided by num1.
+
+// const prompt = require('prompt-sync')();
+// let num = parseInt(prompt("Enter the number"));
+// let num1= parseInt(prompt("Enter the number1"));
+// console.log(parseInt(num/num1));
+
+
+// 3. Take two numbers num,num1 and print the exponential power of the first number raised by the second.
+
+// const prompt =require ('prompt-sync')();
+// let num = parseInt(prompt("Enter the number"));
+// let num1 = parseInt(prompt("Enter the num1"));
+//  console.log(num**num1);
+
+
+// 4. Take two numbers a,b and only print the decimal part of the result obtained when a is divided by b.
+
+// const prompt = require('prompt-sync')();
+// let num = parseInt(prompt("Enter tne number"));
+// let num1 = parseInt(prompt("Enter the number1"));
+// let a = num/num1;
+// let b = a - parseInt(a);
+// console.log(b);
+
+
+// 5. Take marks in 5 subjects A,B,C,D,E and print out the average of marks from five subject marks.
+
+// const prompt = require ('prompt-sync')();
+// let num = parseInt(prompt("Enter the number1"));
+// let num1 = parseInt(prompt("Enter the number2"));
+// let num2 = parseInt(prompt("Enter the number3"));
+// let num3 = parseInt(prompt("Enter the number4"));
+// let num4 = parseInt(prompt("Enter the number5"));
+// let sum = num + num1 + num2 + num3 + num4;
+// let avg = sum/5;
+// console.log(avg);
+
+
+// 6. Take the cost price a and selling price b of a product, and print the profit obtained on the product.
+
+// const prompt = require ('prompt-sync')();
+// let num = parseInt(prompt("Enter the number"));
+// let num1 = parseInt(prompt("Enter the number"));
+// if(num<num1){
+//     console.log("profit");
+// }else{
+//     console.log("loss");
+// }
+
+// 7. Take a number and print out the last digit of the number.
+
+// const prompt = require ('prompt-sync')();
+// let num = parseInt(prompt("Enter the number"));
+// let a = num % 10;
+// console.log(a);
+
+
+// 8. Take a 2 digit number and return the first and last digits.
+
+// const prompt = require ('prompt-sync')();
+// let num = parseInt(prompt("Enter the number"));
+// let a = num / 10;
+// let b = num % 10;
+// console.log("first digits" + parseInt(a));
+// console.log ("last digits" + b);
+
+
+//9. Write a JavaScript program to find the largest of three numbers.
+
+// const prompt = require ('prompt-sync')();
+// let num = parseInt(prompt("Enter the number"));
+// if (num === 999){
+//     console.log("largest");
+// }
+// else{
+//     console.log("NOT");
+// }
+
+
+// 10. Write a JavaScript program to check whether a number is even or odd.
+
+// const prompt = require ('prompt-sync')();
+// let num = parseInt (prompt("Enter the number"));
+// if(num % 2===0){
+//     console.log("Even");
+// }else{
+//     console.log("Odd");
+// }
+
+
+// 11. Write a JavaScript program to check whether a given number is prime or not.
+
+// const prompt = require ('prompt-sync')();
+// let num = parseInt(prompt("Enter the number"));
+// let co = 0;
+// let i;
+// for( i = 1; i<=num; i++){
+//     if(num%i==0){
+//         co = co + 1;
+//     }
+// }
+// if(co==2){
+//     console.log("YES");
+// }else{
+//     console.log("NO");
+// }
+
+
