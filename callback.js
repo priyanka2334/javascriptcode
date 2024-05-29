57e82a9a4c0e5b8999c74ee205a33654a7bf008f
priyanka2334 <priyankadiwakar2024@gmail.com>
2024-05-29 11:03:40 +0530

    hell

--- callback.js
+++ callback.js
@@ -145,3 +145,81 @@
 // }
 // atm();
 
+
+
+//////////////////////////////////////////////////////callback hell
+
+
+function ATM(enter){
+    console.log("visit Atm");
+    enter();
+}
+
+function Enteratm(insert){
+    console.log("enter atm");
+    insert();
+}
+
+function Insertcart(processing){
+    console.log("insert the card");
+    processing();
+}
+
+function Processing(pin){
+    setTimeout(()=>{
+        console.log("processing for withdraw");
+        pin();
+    },5000)
+}
+
+
+function Enterpin(withdraw){
+    setTimeout(()=>{
+        console.log("Please enter the pin");
+        withdraw();
+    },2000)
+}
+
+
+function Withdrawmoney(cash){
+    setTimeout(()=>{
+        console.log("With draw the money");
+        cash();
+    },5000)
+}
+
+
+function CollectCash(Leave){
+    setTimeout(()=>{
+        console.log("Collect the cash");
+        Leave();
+    },1000);
+} 
+
+
+function LeaveATM(leavATM){
+    setTimeout(()=>{
+        console.log("Leave the Atm");
+        leavATM()
+        
+
+    },2000)
+}
+
+
+ATM(()=>{
+    Enteratm(()=>{
+        Insertcart(()=>{
+            Processing(()=>{
+                Enterpin(()=>{
+                    Withdrawmoney(()=>{
+                        CollectCash(()=>{
+                            LeaveATM(()=>{
+                            })
+                        })
+                    })
+                })
+            })
+        })
+    })
+});
